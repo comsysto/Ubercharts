a0b12f7b86146c785ef607697bbc5934aff9c961
Mohammed El Batya <elbatya@googlemail.com>
2011-02-23 15:43:32 +0100

    - fixed and documented CoordinateSeries

--- src/main/java/com/comsysto/insight/model/options/series/impl/CoordinateSeries.java
+++ src/main/java/com/comsysto/insight/model/options/series/impl/CoordinateSeries.java
@@ -3,25 +3,54 @@ package com.comsysto.insight.model.options.series.impl;
 import com.comsysto.insight.model.options.series.generic.AbstractSeries;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
- * Date: Feb 18, 2011
- * Time: 9:11:09 PM
+ * Implementation of {@link CoordinateSeries} which represents an array of X/Y-Coordinates to be plotted on the chart.
+ *
+ * For more information read href="http://www.highcharts.com/ref/#series.
+ *
+ * Date: Feb 18, 2011 Time: 9:11:09 PM
  *
  * @author Mohammed El Batya
  */
 public class CoordinateSeries extends AbstractSeries<Number[][]> {
 
+  /** {@link AbstractSeries#AbstractSeries()} */
+  public CoordinateSeries() {
+  }
+
+  /** {@link AbstractSeries#AbstractSeries(String)} */
   public CoordinateSeries(String pName) {
     super(pName);
   }
 
 
+  /**
+   * Sets a two-dimensional array of X/Y-Coordinates to be plotted on the chart.
+   *
+   * The second dimension of the array must have the length of 2, to represent X/Y coordinate.
+   *
+   * @param coordinates an array of X/Y-Coordinates to be plotted on the chart
+   *
+   * @return this object for convenient chaining, not a copy
+   */
   public CoordinateSeries setData(Number[][] coordinates) {
     data = coordinates;
     return this;
   }
 
+  /**
+   * Sets two arrays to represent X and Y values for coordinates to be plotted on the chart. Both array must have the
+   * same length or an {@link IllegalArgumentException} will be thrown. Every X-Value will be paired with a Y-Value,
+   * stored at the same array index.
+   *
+   * @param pXValues an array representing the X-Values
+   * @param pYValues an array representing the Y-Values
+   *
+   * @return this object for convenient chaining, not a copy
+   * @throws IllegalArgumentException
+   */
   public CoordinateSeries setData(Number[] pXValues, Number[] pYValues) {
 
 
@@ -43,10 +72,27 @@ public class CoordinateSeries extends AbstractSeries<Number[][]> {
 
   }
 
+  /**
+   * Sets a map of X/Y values to represent coordinates to be plotted on the chart. The key will be treated as X-value
+   * and the value as Y-value.
+   *
+   * @param pCoordinates a map of X/Y values to represent coordinates to be plotted on the chart
+   *
+   * @return this object for convenient chaining, not a copy
+   */
   public CoordinateSeries setData(Map<Number, Number> pCoordinates) {
 
-    Number[] xValues = (Number[]) (pCoordinates.keySet()).toArray();
-    Number[] yValues = (Number[]) (pCoordinates.entrySet()).toArray();
+    Set<Map.Entry<Number, Number>> entries = pCoordinates.entrySet();
+
+    Number[] xValues = new Number[entries.size()];
+    Number[] yValues = new Number[entries.size()];
+
+    int i = 0;
+    for (Map.Entry<Number, Number> e : entries) {
+      xValues[i] = e.getKey();
+      yValues[i] = e.getValue();
+      i++;
+    }
 
     return setData(xValues, yValues);
   }
