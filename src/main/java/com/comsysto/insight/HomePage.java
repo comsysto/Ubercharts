80cfdfb63d7f797e5224c46d7f43b7d11d00b92e
Mohammed El Batya <elbatya@googlemail.com>
2011-03-01 13:28:17 +0100

    - added one chart for each chart type to the homepage - each chart is filled with one series for each supportet series type ( mixed series excluded, as it is not working at the moment) - added a little bit "style" to the homepage

--- src/main/java/com/comsysto/insight/HomePage.java
+++ src/main/java/com/comsysto/insight/HomePage.java
@@ -3,9 +3,13 @@ package com.comsysto.insight;
 import com.comsysto.insight.component.HighchartsPanel;
 import com.comsysto.insight.model.Highchart;
 import com.comsysto.insight.model.charts.AreaChart;
+import com.comsysto.insight.model.charts.AreaSplineChart;
 import com.comsysto.insight.model.charts.BarChart;
 import com.comsysto.insight.model.charts.ColumnChart;
 import com.comsysto.insight.model.charts.LineChart;
+import com.comsysto.insight.model.charts.PieChart;
+import com.comsysto.insight.model.charts.ScatterChart;
+import com.comsysto.insight.model.charts.SplineChart;
 import com.comsysto.insight.model.options.Point;
 import com.comsysto.insight.model.options.series.generic.ISeries;
 import com.comsysto.insight.model.options.series.impl.CoordinateSeries;
@@ -14,6 +18,9 @@ import com.comsysto.insight.model.options.series.impl.NumberSeries;
 import com.comsysto.insight.model.options.series.impl.PointSeries;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /** Homepage */
 public class HomePage extends WebPage {
@@ -28,14 +35,13 @@ public class HomePage extends WebPage {
   public HomePage(final PageParameters parameters) {
 
 
-    /* List<String> categories = new ArrayList<String>();
+    List<String> categories = new ArrayList<String>();
     categories.add("Jan");
     categories.add("Feb");
     categories.add("Mar");
     categories.add("Apr");
     categories.add("Mai");
     categories.add("Jun");
-    pHighchart.getXAxis().setCategories(categories);  */
 
 
     ISeries a = new NumberSeries("Number").setData(new Number[]{7.0, 6.9, 9.5, 10.2, 12.2, 13.9});
@@ -48,24 +54,40 @@ public class HomePage extends WebPage {
 
     ISeries e = new PointSeries("Points").setData(new Point[]{new Point(1, 5), new Point(3, 4), new Point(6, 7)});
 
-    Highchart highchart1 = new Highchart(new LineChart());
-    highchart1.addSeries(a);
+    Highchart highchart1 = new Highchart(new AreaChart());
+    highchart1.addSeries(a, c, d, e).getXAxis().setCategories(categories);
 
+    Highchart highchart2 = new Highchart(new AreaSplineChart());
+    highchart2.addSeries(a, c, d, e).getXAxis().setCategories(categories);
 
-    Highchart highchart2 = new Highchart(new AreaChart());
-    highchart2.addSeries(a);
+    Highchart highchart3 = new Highchart(new BarChart());
+    highchart3.addSeries(a, c, d, e).getXAxis().setCategories(categories);
 
-    Highchart highchart3 = new Highchart(new ColumnChart());
-    highchart3.addSeries(a);
+    Highchart highchart4 = new Highchart(new ColumnChart());
+    highchart4.addSeries(a, c, d, e).getXAxis().setCategories(categories);
 
-    Highchart highchart4 = new Highchart(new BarChart());
-    highchart4.addSeries(a);
+    Highchart highchart5 = new Highchart(new LineChart());
+    highchart5.addSeries(a, c, d, e).getXAxis().setCategories(categories);
+
+    Highchart highchart6 = new Highchart(new PieChart());
+    highchart6.addSeries(a, c, d, e).getXAxis().setCategories(categories);
+
+    Highchart highchart7 = new Highchart(new ScatterChart());
+    highchart7.addSeries(a, c, d, e).getXAxis().setCategories(categories);
+
+    Highchart highchart8 = new Highchart(new SplineChart());
+    highchart8.addSeries(a, c, d, e).getXAxis().setCategories(categories);
+
 
     // Add Panel
     add(new HighchartsPanel("chart1", highchart1));
     add(new HighchartsPanel("chart2", highchart2));
     add(new HighchartsPanel("chart3", highchart3));
     add(new HighchartsPanel("chart4", highchart4));
+    add(new HighchartsPanel("chart5", highchart5));
+    add(new HighchartsPanel("chart6", highchart6));
+    add(new HighchartsPanel("chart7", highchart7));
+    add(new HighchartsPanel("chart8", highchart8));
   }
 
 
