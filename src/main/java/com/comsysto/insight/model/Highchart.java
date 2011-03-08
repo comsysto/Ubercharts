040c8bc4826db036dfaec12259674a62ee89f269
Mohammed El Batya <elbatya@googlemail.com>
2011-03-08 13:26:31 +0100

    - created new branch "jackson" and switched to jackson as serialization lib

--- src/main/java/com/comsysto/insight/model/Highchart.java
+++ src/main/java/com/comsysto/insight/model/Highchart.java
@@ -4,108 +4,95 @@ import com.comsysto.insight.model.charts.Chart;
 import com.comsysto.insight.model.options.Title;
 import com.comsysto.insight.model.options.XAxis;
 import com.comsysto.insight.model.options.series.generic.ISeries;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
-import java.lang.reflect.Type;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Highchart {
 
-  // TODO: refactor this
-  // 1. find minimal configuration needed to render a chart (AFAIK chart, series and xAxis is minimum)
-  // 2. group similar charts by type (e.g. column and bar charts are same (?))
-  // 3. all attributes should be private
-  // 4. make "builder-pattern light" structure of all objects
-  // 5. write some documentation
+    // TODO: refactor this
+    // 1. find minimal configuration needed to render a chart (AFAIK chart, series and xAxis is minimum)
+    // 2. group similar charts by type (e.g. column and bar charts are same (?))
+    // 3. all attributes should be private
+    // 4. make "builder-pattern light" structure of all objects
+    // 5. write some documentation
 
 
-  private static Gson mGson = new GsonBuilder().setPrettyPrinting()
-    .registerTypeAdapter(ISeries.class, new SeriesSerializer())
-    .create();
+    private ObjectMapper mapper = new ObjectMapper().configure(SerializationConfig.Feature.WRITE_NULL_PROPERTIES, false);
 
 
-  private Chart chart;
-  private XAxis xAxis = new XAxis();
-  private Title title;
+    private Chart chart;
+    private XAxis xAxis = new XAxis();
+    private Title title;
 
 
-  //private List<ISeries> series;
-  private List<ISeries> series = new LinkedList<ISeries>();
+    //private List<ISeries> series;
+    private List<ISeries> series = new LinkedList<ISeries>();
 
 
-  public Highchart(Chart pChart) {
-    chart = pChart;
-  }
-
-
-  public String toJson() {
-
-
-    String json = "";
-
-    json = mGson.toJson(this);
-
-    // dump JSON object
-    System.out.println(json);
-
-    return json;
-  }
-
-  public Chart getChart() {
-    return chart;
-  }
-
-  public Highchart addSeries(ISeries... pSeries) {
-
-    for (ISeries s : pSeries) {
-      series.add(s);
+    public Highchart(Chart pChart) {
+        chart = pChart;
+        mapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
     }
 
-    return this;
-  }
 
-  public Highchart setSeries(ISeries... pSeries) {
-    series.clear();
-    addSeries(pSeries);
-    return this;
-  }
-
-  public XAxis getXAxis() {
-    return xAxis;
-  }
-
-  public Title getTitle() {
-    return title;
-  }
-
-  public Highchart setTitle(Title pTitle) {
-    title = pTitle;
-    return this;
-  }
-
-  private static class SeriesSerializer implements JsonSerializer<ISeries> {
-
-    private Gson mGson;
-    private JsonParser mParser = new JsonParser();
+    public String toJson() {
 
 
-    SeriesSerializer() {
+        String json = "";
 
-      GsonBuilder builder = new GsonBuilder().setPrettyPrinting();
-      mGson = builder.create();
+        try {
+            json = mapper.writeValueAsString(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // dump JSON object
+        System.out.println(json);
+
+        return json;
     }
 
-    public JsonElement serialize(ISeries src, Type typeOfSrc, JsonSerializationContext context) {
-      return mParser.parse(mGson.toJson(src));
+    public Chart getChart() {
+        return chart;
     }
-  }
+
+    public Highchart addSeries(ISeries... pSeries) {
+
+        for (ISeries s : pSeries) {
+            series.add(s);
+        }
+
+        return this;
+    }
+
+    public Highchart setSeries(ISeries... pSeries) {
+        series.clear();
+        addSeries(pSeries);
+        return this;
+    }
+
+
+    public List<ISeries> getSeries() {
+        return series;
+    }
+
+    public XAxis getXAxis() {
+        return xAxis;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public Highchart setTitle(Title pTitle) {
+        title = pTitle;
+        return this;
+    }
 
 
 }
--- src/main/java/com/comsysto/insight/model/options/Point.java
+++ src/main/java/com/comsysto/insight/model/options/Point.java
@@ -7,16 +7,63 @@ package com.comsysto.insight.model.options;
  */
 public class Point {
 
-  private String color;
-  private String id;
-  private String name;
-  private boolean sliced;
-  private Number x;
-  private Number y;
+    private String color;
+    private String id;
+    private String name;
+    private boolean sliced;
+    private Number x;
+    private Number y;
 
-  public Point(Number pX, Number pY) {
-    x = pX;
-    y = pY;
-  }
+    public Point(Number pX, Number pY) {
+        x = pX;
+        y = pY;
+    }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSliced() {
+        return sliced;
+    }
+
+    public void setSliced(boolean sliced) {
+        this.sliced = sliced;
+    }
+
+    public Number getX() {
+        return x;
+    }
+
+    public void setX(Number x) {
+        this.x = x;
+    }
+
+    public Number getY() {
+        return y;
+    }
+
+    public void setY(Number y) {
+        this.y = y;
+    }
 }
