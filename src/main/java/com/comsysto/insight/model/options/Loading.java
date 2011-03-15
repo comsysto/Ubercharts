78030f40115dbd54d321837da30fd6fb1a8e9ce0
Mohammed El Batya <elbatya@googlemail.com>
2011-03-15 15:58:52 +0100

    - changed Loading.java to support chaining instead of builder pattern

--- src/main/java/com/comsysto/insight/model/options/Loading.java
+++ src/main/java/com/comsysto/insight/model/options/Loading.java
@@ -4,68 +4,44 @@ import java.util.Map;
 
 public class Loading {
 
-  private final Integer hideDuration;
-  private final Map<String, String> labelStyle;
-  private final Integer showDuration;
-  private final Map<String, String> style;
+    private Integer mHideDuration;
+    private Map<String, String> mLabelStyle;
+    private Integer mShowDuration;
+    private Map<String, String> mStyle;
 
-  private Loading(Builder builder) {
-    this.hideDuration = builder.hideDuration;
-    this.labelStyle = builder.labelStyle;
-    this.showDuration = builder.showDuration;
-    this.style = builder.style;
-  }
-
-  public Integer getHideDuration() {
-    return hideDuration;
-  }
-
-  public Map<String, String> getLabelStyle() {
-    return labelStyle;
-  }
-
-  public Integer getShowDuration() {
-    return showDuration;
-  }
-
-  public Map<String, String> getStyle() {
-    return style;
-  }
-
-
-  public static class Builder {
-
-    private Integer hideDuration = 100;
-    private Map<String, String> labelStyle;
-    private Integer showDuration = 100;
-    private Map<String, String> style;
-
-    public Builder() {
+    public Integer getHideDuration() {
+        return mHideDuration;
     }
 
-    public Builder hideDuration(Integer hideDuration) {
-      this.hideDuration = hideDuration;
-      return this;
+    public Loading setHideDuration(Integer pHideDuration) {
+        mHideDuration = pHideDuration;
+        return this;
     }
 
-    public Builder labelStyle(Map<String, String> labelStyle) {
-      this.labelStyle = labelStyle;
-      return this;
+    public Map<String, String> getLabelStyle() {
+        return mLabelStyle;
     }
 
-    public Builder showDuration(Integer showDuration) {
-      this.showDuration = showDuration;
-      return this;
+    public Loading setLabelStyle(Map<String, String> pLabelStyle) {
+        mLabelStyle = pLabelStyle;
+        return this;
     }
 
-    public Builder style(Map<String, String> style) {
-      this.style = style;
-      return this;
+    public Integer getShowDuration() {
+        return mShowDuration;
     }
 
-    public Loading build() {
-      return new Loading(this);
+    public Loading setShowDuration(Integer pShowDuration) {
+        mShowDuration = pShowDuration;
+        return this;
     }
-  }
 
+    public Map<String, String> getStyle() {
+        return mStyle;
+    }
+
+    public Loading setStyle(Map<String, String> pStyle) {
+        mStyle = pStyle;
+        return this;
+    }
 }
