42751e2f782f24544766e69305588f936669a730
Luka Leovac <luka.leovac@comsysto.com>
2012-09-19 16:15:28 +0200

    Added Stacking to PlotOptions

---
+++ ubercharts-lib/src/main/java/com/comsysto/insight/model/options/Stacking.java
@@ -0,0 +1,13 @@
+package com.comsysto.insight.model.options;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: berndzuther
+ * Date: 13.07.12
+ * Time: 18:56
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Stacking {
+    normal,
+    percent
+}
